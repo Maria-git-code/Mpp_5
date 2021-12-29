710108738833585c4aa3e2c74ade532302f57d63
Maria-git-code <83037470+Maria-git-code@users.noreply.github.com>
2021-12-29 15:48:07 +0300

    Add files via upload

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0]; //"C:\\Users\\Пользователь\\RiderProjects\\lab5\\lab5Ex\\bin\\Debug\\net5.0\\lab5Ex.dll";                                               //Assembly.GetExecutingAssembly().Location;
+            
+            Assembly.LoadFrom(path).GetTypes()
+                .Where(type => type.IsPublic)
+                .OrderBy(type => type.FullName)
+                .ToList()
+                .ForEach(type => Console.WriteLine(type.FullName));
+        }
+    }
+}
+
+
+
---
+++ lab5/lab5Ex/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab5Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+namespace A
+{
+    public class Hello
+    {
+    }
+
+    class Anything
+    {
+    }
+
+    public class B
+    {
+    }
+}
